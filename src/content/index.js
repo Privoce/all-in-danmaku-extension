fa82d075e080ea3d733a9bcce33e510c2f5fdb13
Curtis Men <curtis_1020@hotmail.com>
2021-07-01 22:59:00 +0800

    alpha release 0.2.0

--- src/content/index.js
+++ src/content/index.js
@@ -14,6 +14,9 @@ const YoutubePlayerState = {
     "VIDEO_CUED": 5
 };
 
+let globalDanmakuFetched = 0;
+
+
 function getVideoPlayer() {
     let videos = Array.from(document.querySelectorAll('video')).filter((v) => !!v.src);
     let largestVideoSize = 0;
@@ -32,6 +35,9 @@ function getVideoPlayer() {
 
 const VT = getVideoPlayer()
 let playBackIndex = 0
+
+console.log(VT.style.width)
+console.log(VT.style.height)
 
 console.log("Hello World");
 
@@ -90,6 +96,30 @@ function Danmaku(props) {
     )
 }
 
+function PageSwitcher(props) {
+    return (
+        <div className="danmaku-page-switcher" style={{marginTop: '10px'}}>
+            <span className="switcher-text">Danmaku Switcher:</span>
+            <div className="danmaku-page-switcher-inner">
+                <label className="switch" style={{marginLeft: '5px'}}>
+                    <input type="checkbox" id="all-in-danmaku-switcher"/>
+                    <span className="slider round"></span>
+                </label>
+            </div>
+        </div>
+    )
+}
+
+const infoBar = document.getElementById('info')
+const toolBar = document.createElement('div')
+const referenceBar = document.getElementById('info-contents')
+toolBar.id = 'all-in-danmaku-toolbar'
+infoBar.insertBefore(toolBar, referenceBar)
+
+ReactDOM.render(<PageSwitcher />, toolBar)
+
+const switcher = document.getElementById('all-in-danmaku-switcher')
+
 class DanmakuLayer extends React.Component {
     constructor(props) {
         super(props);
@@ -99,7 +129,9 @@ class DanmakuLayer extends React.Component {
             danmakuList: null,
             timeStamp: 0,
             screen: null,
-            intervalID: null
+            intervalID: null,
+            width: '500px',
+            height: '300px'
         }
         // this.tryGetDanmaku()
         // TODO: sendMessage to background, background start the actual request and store contents in localStorage,
@@ -108,48 +140,93 @@ class DanmakuLayer extends React.Component {
     }
 
     componentDidMount() {
-        this.tryGetDanmaku()
+        /*const resizeObserver = new ResizeObserver(entries => {
+            for (let entry of entries) {
+                if (entry.width) {
+                    this.state.width = entry.width.toString() + 'px'
+                }
+                if (entry.height) {
+                    this.state.height = entry.height.toString() + 'px'
+                }
+            }
+        })
+        resizeObserver.observe(VT)*/
+        switcher.addEventListener('change', (event) => {
+            if (event.currentTarget.checked) {
+                if (globalDanmakuFetched === 0) {
+                    this.tryGetDanmaku()
+                } else {
+                    if (this.state.screen) {
+                        this.state.screen.show()
+                    }
+                }
+            } else {
+                if (this.state.screen) {
+                    this.state.screen.hide()
+                }
+            }
+        })
         VT.onpause = () => {
             console.log('pause')
-            this.state.screen.pause()
+            if (this.state.screen) {
+                this.state.screen.pause()
+            }
+
         }
         VT.onplay = () => {
             console.log('play')
-            this.state.screen.resume()
+            if (this.state.screen) {
+                this.state.screen.resume()
+            }
+
         }
         VT.ontimeupdate = () => {
-            const newIndex = this.state.danmakuList.findIndex((element) =>
-                element.progress > Math.floor(VT.currentTime * 1000)
-            )
-            if (newIndex === -1) return;
-            if (this.state.danmakuList[newIndex].progress - this.state.danmakuList[playBackIndex].progress > 10000
-                || newIndex < playBackIndex) { // 10s
-                console.log('refresh')
-                this.state.screen.clear()
-                playBackIndex = newIndex
+            if (Array.isArray(this.state.danmakuList) && this.state.screen) {
+                const newIndex = this.state.danmakuList.findIndex((element) =>
+                    element.progress > Math.floor(VT.currentTime * 1000)
+                )
+                if (newIndex === -1) return;
+                if (this.state.danmakuList[newIndex].progress - this.state.danmakuList[playBackIndex].progress > 10000
+                    || newIndex < playBackIndex) { // 10s
+                    console.log('refresh')
+                    this.state.screen.clear()
+                    playBackIndex = newIndex
 
-            } else {
-                console.log('push')
-                while (playBackIndex < newIndex) {
-                    this.state.screen.push(
-                        <StyledBullet
-                            msg={this.state.danmakuList[playBackIndex].content}
-                            size='small'
-                            color={this.state.danmakuList[playBackIndex].color}
-                        />
-                    );
-                    playBackIndex += 1;
+                } else {
+                    console.log('push')
+                    while (playBackIndex < newIndex) {
+                        this.state.screen.push(
+                            <StyledBullet
+                                size="small"
+                                msg={this.state.danmakuList[playBackIndex].content}
+                            />
+                        );
+                        playBackIndex += 1;
+                    }
                 }
             }
         }
+        VT.onloadeddata = () => {
+            this.setState({
+                width: VT.style.width,
+                height: VT.style.height
+            })
+            /*this.state.width = VT.style.width
+            this.state.height = VT.style.height*/
+        }
     }
+
+    /*componentDidUpdate(prevProps, prevState, snapshot) {
+        const s = new BulletScreen(document.querySelector('.screen'), {duration: 6})
+        this.setState({screen: s})
+    }*/
 
     onSwitchClicked() {
 
     }
 
     tryGetDanmaku() {
-        this.state.bvId = 'BV1F54y1G7mi'
+        // this.state.bvId = 'BV1F54y1G7mi'
         //TODO: do some search
         let newBVId = prompt("Please Enter BV ID: ")
         this.state.bvId = newBVId
@@ -167,12 +244,13 @@ class DanmakuLayer extends React.Component {
                             }
                             return 0;
                         })
-                        this.state.screen = new BulletScreen(document.querySelector('.screen'), {duration: 10})
-                        this.state.screen.push(<StyledBullet msg={this.state.danmakuList[0].content} />)
+                        this.state.screen = new BulletScreen(document.querySelector('.screen'), {duration: 6})
+                        // this.state.screen.push(<StyledBullet msg={this.state.danmakuList[0].content} />)
                     }
                     console.log('success')
                     console.log(this.state.danmakuList)
                     console.log(Array.isArray(this.state.danmakuList))
+                    globalDanmakuFetched = 1
                 })
             }
         })
@@ -185,7 +263,7 @@ class DanmakuLayer extends React.Component {
 
     render() {
         return (
-            <div className='screen' style={{width: '100vw', height: '80vh', zIndex: 15}}></div>
+            <div className="screen" style={{ width: this.state.width, height: this.state.height, zIndex: 15 }}></div>
         )
     }
 }
