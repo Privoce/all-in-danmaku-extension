e50f3dcd77287a47d91696aae57afbf67688b6dd
TBDGF <rtbdgf@163.com>
2021-07-14 16:44:16 +0800

    sidebar update

--- src/api/StyledDanmaku.js
+++ src/api/StyledDanmaku.js
@@ -18,11 +18,9 @@ const sizes = {
 };
 
 const StyledDanmaku = ({ msg, size = 'small', color , alpha}) => {
-    console.log("#0 msg get "+color+" "+alpha)
     MsgStyle.opacity=alpha;
     MsgStyle.color=color;
     MsgStyle.fontSize=sizes[size];
-    // console.log("#1 msg get "+MsgStyle.opacity+" "+MsgStyle.color+" "+MsgStyle.fontSize)
     return (
         <div>
             <div style={MsgStyle}>{msg}</div>
